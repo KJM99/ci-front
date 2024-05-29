a1137b114920e976c4a8e8041e08b4f1e04ae7fc
Kim JaeMin <2098kjm@naver.com>
2024-05-29 14:11:30 +0900

    외부ID 수정

--- src/config/network.js
+++ src/config/network.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = async (url, method, body) => {
-  axios.defaults.baseURL = "http:///35.193.99.200:8080/api/v1/";
+  axios.defaults.baseURL = "http:///34.173.197.218:8080/api/v1/";
   // axios.defaults.baseURL = "http://146.148.107.161:8080/api/";
   const res = await axios({
     url,
