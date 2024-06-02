c2523041ae01d3d535ffdb9680f7db794ae2d214
Kim JaeMin <2098kjm@naver.com>
2024-06-02 21:58:00 +0900

    front

--- src/api/board.js
+++ src/api/board.js
@@ -1,10 +1,23 @@
 import { api } from "../config/network";
 
 export const getBoard = async () => {
-  const res = await api("/api/v1/boards", "get");
+  const res = await api("/api/v1/boards", "get", "", 1);
   return res;
 };
 
 export const addBoard = async (data) => {
-  await api("/api/v1/boards", "post", data);
+  await api("/api/v1/boards", "post", data, 1);
+};
+
+export const getComment = async () => {
+  const res = await api("/comments", "get", "", 2);
+  return res;
+};
+
+export const addBoardComment = async (data) => {
+  await api("/comments", "post", data, 2);
+};
+
+export const deleteComment = async (data) => {
+  await api("/comments", "delete", data, 2);
 };
--- src/page/Board.jsx
+++ src/page/Board.jsx
@@ -1,22 +1,33 @@
 import { useEffect, useState } from "react";
-import { addBoard, getBoard } from "../api/board";
+import {
+  addBoard,
+  addBoardComment,
+  deleteComment,
+  getBoard,
+  getComment,
+} from "../api/board";
 
 const Board = () => {
   const [name, setName] = useState(null);
   const [text, setText] = useState(null);
   const [viewData, setViewData] = useState([]);
+  const [comment, setComment] = useState(null);
+  const [commentData, setCommentData] = useState([]);
+  const [inputData, setInputData] = useState([]);
 
   const fetchData = async () => {
     try {
-      const response = await getBoard();
-      console.log(response);
-      setViewData(response.data);
+      const response1 = await getBoard();
+      setViewData(response1.data);
+      const response2 = await getComment();
+      setCommentData(response2.data);
+      console.log(commentData);
     } catch (error) {
       console.error("Error: ", error);
     }
   };
 
-  const submitData = async (e) => {
+  const addData = async (e) => {
     try {
       e.preventDefault();
       const data = { name, text };
@@ -26,6 +37,35 @@ const Board = () => {
       fetchData();
     } catch (error) {
       console.log(error.response?.data.message);
+    }
+  };
+
+  const onClickDelete = async (commentId, boardId) => {
+    try {
+      const del = { commentId, boardId };
+      await deleteComment(del);
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addComment = async (boardId) => {
+    if (comment === null) {
+      alert("댓글을 입력해주세요!");
+      return;
+    }
+    const update = [...inputData];
+    update[boardId] = null;
+    try {
+      const com = { boardId, comment };
+      // post 요청
+      await addBoardComment(com);
+      setComment(null);
+      setInputData(update);
+      fetchData();
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -39,10 +79,20 @@ const Board = () => {
     fetchData();
   }, []);
 
+  const handleInputChange = (e, index) => {
+    const update = [...inputData];
+    update[index] = e.target.value;
+    setComment(update[index]);
+    setInputData(update);
+  };
+
   return (
     <>
-      <div>
-        <label htmlFor="name" />
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
         <input
           id="name"
           type="text"
@@ -50,7 +100,6 @@ const Board = () => {
           onChange={(e) => setName(e.target.value)}
           value={name === null ? "" : name}
         />
-        <label htmlFor="text" />
         <input
           id="text"
           type="text"
@@ -58,16 +107,67 @@ const Board = () => {
           onChange={(e) => setText(e.target.value)}
           value={text === null ? "" : text}
         />
-      </div>
-      <div>
-        <button onClick={submitData}>submit</button>
+        <button onClick={addData}>add</button>
       </div>
       <div>
         {viewData.map((el, i) => (
-          <div key={i}>
-            <p>
-              <b>{el.name}</b> : {el.text}
+          <div
+            key={i}
+            style={{
+              border: "1px solid black",
+            }}
+          >
+            <p style={{ marginLeft: "1rem" }}>
+              <b>이름 : {el.name}</b>
             </p>
+            <p style={{ marginLeft: "1rem" }}>내용 : {el.text}</p>
+            <hr />
+            <div>
+              {commentData
+                .filter((comment) => comment.boardId === i)
+                .map((el1, j) => (
+                  <div
+                    key={j}
+                    style={{
+                      marginLeft: "2rem",
+                      marginRight: "2rem",
+                      display: "flex",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <p> ┗ {el1.comment}</p>
+                    <button
+                      style={{
+                        width: "1rem",
+                      }}
+                      onClick={() => onClickDelete(el1.id, i)}
+                    >
+                      X
+                    </button>
+                  </div>
+                ))}
+            </div>
+            <hr />
+            <div
+              key={i}
+              style={{
+                marginLeft: "1rem",
+                display: "flex",
+                marginBottom: "0.5rem",
+              }}
+            >
+              <input
+                id="comment"
+                type="text"
+                placeholder="comment"
+                // onChange={(e) => setComment(e.target.value)}
+                onChange={(e) => handleInputChange(e, i)}
+                // value={comment === null ? "" : comment}
+                value={inputData[i] || ""}
+                style={{ width: "-webkit-fill-available" }}
+              />
+              <button onClick={() => addComment(i)}>Comment</button>
+            </div>
           </div>
         ))}
       </div>
